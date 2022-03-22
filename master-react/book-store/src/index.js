307b85de6ffe6826fbbed5f56a53e79626ba8500
w <s3864077@rmit.edu.vn>
2022-03-22 16:00:02 +0700

    cho Quan

--- master-react/book-store/src/index.js
+++ master-react/book-store/src/index.js
@@ -1,47 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51lKcHdQvJL._AC_UL906_SR906,600_.jpg",
+    title: "I Love You to the Moon",
+    author: "Amelia Hepworth",
+  },
 
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/51lKcHdQvJL._AC_UL906_SR906,600_.jpg",
-  title: "I Love You to the Moon",
-  author: "Amelia Hepworth",
-};
-
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/91DUhtU6KCL._AC_UL906_SR906,600_.jpg",
-  title: "The Lincoln Highway: A Novel",
-  author: "Amor Towles",
-};
-//Set up vars
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91DUhtU6KCL._AC_UL906_SR906,600_.jpg",
+    title: "The Lincoln Highway: A Novel",
+    author: "Amor Towles",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        // return <Book key={book.id} book={book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = (props) => {
+// props destrcutung input
+const Book = ({img, title, author}) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("Cho Quan");
+  };
+  const complexExample = () =>{
+
+  }
+  // //props destructing
+  // const { img, title, author } = props.book;
+
   return (
     <article className="book">
-      <img className="image" src={props.img} alt="" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img className="image" src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}> Example</button>
+      <button type=""></button>
     </article>
   );
 };
